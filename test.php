56793f56f27f9115066d19dd6a86489b85baeb68
Max Base <MaxBaseCode@Gmail.Com>
2020-05-19 21:43:52 +0430

    Update test.php

--- test.php
+++ test.php
@@ -1,40 +1,15 @@
 <?php
 include "_core.php";
-$url=buildQuery([
+$res=call("create", [
 	"allowStartStopRecording"=>true,
 	"attendeePW"=>"ap",
 	"autoStartRecording"=>false,
-	"meetingID"=>"random-4302986",
+	"meetingID"=>"random-4302986".rand(10,9999),
 	"moderatorPW"=>"mp",
-	"name"=>"random-4302986",
+	"name"=>"random-4302986".rand(10,9999),
 	"record"=>false,
 	"voiceBridge"=>75501,
 	"welcome"=>"<br>Welcome to <b>%%CONFNAME%%</b>!",
-	// "checksum"=>"4bb18b6cac3bf5fe**d*****e635e80acf24",
 ]);
-$url="name=Test+Meeting&meetingID=abc123&attendeePW=111222&moderatorPW=333444";
-// print $host."api/create?".$url."\n";
-$method="create";
-$url=$url."&checksum=".sha1($method . $url . $key);
-print $url."\n";
-$res=get($host."api/".$method."?".$url);
+$room=$res;
 print_r($res);
-/*
-<response>
-<returncode>SUCCESS</returncode>
-<meetingID>random-****86</meetingID>
-<internalMeetingID>****-1589900446510</internalMeetingID>
-<parentMeetingID>bbb-none</parentMeetingID>
-<attendeePW>ap</attendeePW>
-<moderatorPW>mp</moderatorPW>
-<createTime>1589900446510</createTime>
-<voiceBridge>75501</voiceBridge>
-<dialNumber>613-555-1234</dialNumber>
-<createDate>Tue May 19 19:30:46 IRDT 2020</createDate>
-<hasUserJoined>false</hasUserJoined>
-<duration>0</duration>
-<hasBeenForciblyEnded>false</hasBeenForciblyEnded>
-<messageKey></messageKey>
-<message></message>
-</response>
-*/
